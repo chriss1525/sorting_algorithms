d0fe40c8bb9fa527f9560346fe0e973f14103478
chriss1525 <okothchris15@gmail.com>
2023-04-23 15:34:33 +0300

    sort an array of integers in ascending order

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,35 +2,39 @@
 #include <stddef.h>
 #include <string.h>
 /**
- *
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size: the size of the array
+ * Return: nothing
  */
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;
-    size_t j;
-    size_t tmp;
-    int tswap = 0;
+	size_t i;
+	size_t j;
+	size_t tmp;
+	int tswap;
 
-    /*make sure the loop comes back until entire list is sorted*/
-    for (i = 0; i < size; i++)
-    {
-        /*iterate through arra swapping items*/
-        for (j = 0; j < size; j++)
-        /* swap items if a number is greater than the next*/
-        {
-            if (array[j] > array[j + 1])
-            {
-                tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
-                tswap = 1;
-                print_array(array, size);
-            }
-        }
-        if (!tswap)
-        {
-            break;
-        }
-    }
+	/*make sure the loop comes back until entire list is sorted*/
+	for (i = 0; i < size; i++)
+	{
+		tswap = 0;
+		/*iterate through arra swapping items*/
+		for (j = 0; j < size; j++)
+		/* swap items if a number is greater than the next*/
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				tswap = 1;
+				print_array(array, size);
+			}
+		}
+		if (!tswap)
+		{
+			break;
+		}
+	}
 }
