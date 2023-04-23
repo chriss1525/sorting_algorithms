753959c65d2e1c08a1ac7bee1613ff188095ad08
chriss1525 <okothchris15@gmail.com>
2023-04-23 19:21:02 +0300

    insertion method

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - inserts integers into a doubly linked list
+ * in ascending order
+ * @list: doubly linked list
+ *
+ * Return: nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		temp = current;
+
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+
+			if (temp->prev == NULL)
+				*list = temp;
+			else
+				temp->prev->next = temp;
+
+			print_list(*list);
+		}
+
+		current = current->next;
+	}
+}
