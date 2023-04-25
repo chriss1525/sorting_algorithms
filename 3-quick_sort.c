6fc775cf49f23e3ab4765f7571e5c9efef6fbe08
chriss1525 <okothchris15@gmail.com>
2023-04-25 09:52:38 +0300

    sort array in ascending order using quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+/**
+ * quick_sort -  sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size: size of an array
+ *
+ *
+ * Return: nothing
+ */
+
+void quick_sort(int *array, size_t size)
+{
+    int pivot;
+    int i;
+    int j;
+
+    if (size < 2)
+        return;
+
+    pivot = array[size / 2];
+
+    for (i = 0, j = size - 1;; i++, j--)
+    {
+        while (array[i] < pivot)
+            i++;
+        while (array[j] > pivot)
+            j--;
+
+        if (i >= j)
+            break;
+
+        swap(&array[i], &array[j]);
+    }
+
+    print_array(array, size);
+    quick_sort(array, i);
+    quick_sort(array + i, size - i);
+}
+
+/**
+ * swap - swaps two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return:nothing
+ */
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
