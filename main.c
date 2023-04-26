a8b558707011f7a6a3343daf4d97a85375315d4d
chriss1525 <okothchris15@gmail.com>
2023-04-26 09:26:00 +0300

    consider list == NULL

--- main.c
+++ main.c
@@ -9,13 +9,6 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    quick_sort(array, n);
-    printf("\n");
-    print_array(array, n);
+    insertion_sort_list(NULL);
     return (0);
 }
